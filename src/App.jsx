2fe3514a2ad9fda7b0f480cb47fff4d52ab4ee30
Arron.Reed <arron.reed@chasacademy.se>
2024-01-04 23:39:16 +0100

    commit and ready to test deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import "./app.scss"
+import "./App.scss"
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Parallax from "./components/parallax/Parallax";
+import Portfolio from "./components/portfolio/Portfolio";
 
 const App = () => {
   return <div>
@@ -8,12 +10,10 @@ const App = () => {
       <Navbar/>
       <Hero/>  
     </section>
-    <section id="Parallex">Parallax</section>
-    <section id="Services">Services</section>
-    <section>Parallax</section>
-    <section id="Portfolio">Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
+    <section id="Services"><Parallax type="services"/></section>
+    <section>Services</section>
+    <section id="Portfolio"><Parallax type="portfolio"/></section>
+    <Portfolio/>
     <section id="Contact">Contact</section>
   </div>;
 };
